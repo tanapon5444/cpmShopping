f9f50f7e4ee04a1b08b4e739aaf3f7dd9e96a5cb
Chayanon Kiadkamchonsakul_SINGHA <chayanon.k@aware.co.th>
2024-04-29 01:47:21 +0700

    fix: downloadPDF button

--- src/script.js
+++ src/script.js
@@ -4,35 +4,40 @@ var product = [{
     name: 'e-studio2329A',
     price: 7000,
     description: ' คุณสมบัติในการถ่ายเอกสาร 1.ความเร็วในการถ่ายเอกสาร 23 แผ่นต่อนาที (A4) 2.ใช้เวลาอุ่นเครื่อง 15 วินาที / หน่วยความจามาตรฐาน 512 MB 3.ความเร็วในการถ่ายเอกสารแผ่นแรก 6.4 วินาที 4.ความละเอียดในการถ่ายเอกสาร 600 x 600 dpi 5.สามารถถ่ายเอกสารต่อเนื่อง 1–999 แผ่นโดยอ่านต้นฉบับเพียงครั้งเดียว 6.หน้าจอแสดงผลแบบ LCD สามารถปรับความเข้มจางหน้าจอได้  7.ถาดบรรจุกระดาษ 250 แผ่น พร้อมถาดด้านข้างวางได้ 100 แผ่น 8.ปรับความเข้ม -จางอัตโนมัติและปรับตามต้องการได้ถึง 9 ระดับ 9.ระบบย่อ -ขยาย 25% - 400% และปรับเพิ่ม - ลดได้ครั้งละ 1% 10.รองรับต้นฉบับเอกสารและสาเนาตั้งแต่ขนาด A5 - A3 11.ใช้หมึกหมุนเวียน 100% ไม่มีกากหมึกทิ้ง 12.รองรับความหนาของกระดาษสาเนาได้ตั้งแต่ 52 - 216 แกรม 13.ชุดป้อนต้นฉบับหน้าหลังอัตโนมัติ (RADF) 50 แผ่น 14.มีชุดกลับสาเนาหน้าหลังอัตโนมัติ (Duplex) 15.สามารถปรินท์ผ่านระบบ WIFI ได้  16.มีฟังก์ชั่นถ่ายบัตรประชาชนหน้า-หลัง ลงสาเนาหน้าเดียวกัน (ID Card) 17.สามารถตั้งรหัสผู้ใช้งานได้ 99 รหัส โดยใช้เลขรหัส 5 หลัก (Department Code) 18.มีระบบถ่ายแยกเรียงชุดสาเนาเอกสารได้ถึง 999 ชุด พร้อมทั้งแบบทับซ้อนและไขว้ 90 องศา (Sort & Rotate Sort) คุณสมบัติในการปริ้นท์ 1.ความเร็วในการพิมพ์ 23 แผ่นต่อนาที (A4) 2.ความละเอียดในการพิมพ์ 2400 x 600 dpi (with smoothing) 3.สั่งสแกนเข้าโทรศัพท์มือถือผ่านสาย USB 4.ภาษาที่ใช้ในการพิมพ์เป็นระบบ Network LAN (10/100 Base T),USB 2.0 Hi Speed, PCL6 (PCL XL), PS3 (Option) 5.รองรับระบบปฏิบัติการ Windows 7, 8.1, 10 (32/64 bit),Server2016, 2012/R2(64 bit), 2008 SP2(32/64 bit),Mac OS 10.11/10.12/10.13/10.14 6.สามารถปรินท์เอกสารหน้า-หลังได้ ใหญ่สุดถึง A3 คุณสมบัติในการสแกนสีและขาว-ดำ 1. ความเร็วในการสแกน 25 แผ่น ต่อนาที (ขาว/ดำ) และ24 แผ่น ต่อนาที (สี), (A4) และสแกนเอกสารได้ถึงขนาด A3 2.ความละเอียดในการสแกน 600 x 600 dpi, สแกนเอกสารจัดเก็บ ใน USB Thumb Drive โดยตรง ทั้งสีและขาวดำ  3.คำสั่งในการสแกน Scan to File, Scan to E-mail, Scan to USB, Scan to SMB, FTP  4.ชนิดของไฟล์ ในการสแกน PDF, TIFF, JPEG.',
-    type: 'Printer-Bw'
+    type: 'Printer-Bw',
+    linkDownload: ''
 }, {
     id: 2,
     img: 'asset/products/e2520AC,3025,4525.jpg',
     name: 'e-studio2520AC',
     price: 1500,
     description: 'Printer-color Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
-    type: 'Printer-color'
+    type: 'Printer-color',
+    linkDownload: '18f1XgOxsBYNqOv920HZUBKdap2b5l1zt'
 }, {
     id: 3,
     img: 'asset/products/BPS150.PNG',
     name: 'BPS-150',
     price: 110000,
     description: 'Blue-printer-solution Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
-    type: 'Blue-printer-solution'
+    type: 'Blue-printer-solution',
+    linkDownload: ''
 }, {
     id: 4,
     img: 'asset/products/as1500.png',
     name: 'AURORA AS-1500CD',
     price: 45000,
     description: 'AURORA AS Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
-    type: 'AURORA'
+    type: 'AURORA',
+    linkDownload: ''
 }, {
     id: 5,
     img: 'asset/products/2523AD.jpg',
     name: 'e-studio2523AD',
     price: 45000,
     description: 'Printer Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
-    type: 'Printer-Bw'
+    type: 'Printer-Bw',
+    linkDownload: ''
 
 }, {
     id: 6,
@@ -40,106 +45,82 @@ var product = [{
     name: 'e-studio3028A',
     price: 45000,
     description: 'Printer Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
-    type: 'Printer-Bw'
+    type: 'Printer-Bw',
+    linkDownload: ''
 }, {
     id: 7,
     img: 'asset/products/e4528A.jpg',
     name: 'e-studio4528A',
     price: 45000,
     description: 'Printer Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
-    type: 'Printer-Bw'
+    type: 'Printer-Bw',
+    linkDownload: ''
 }, {
     id: 8,
     img: 'asset/products/e5528A.jpg',
     name: 'e-studio5528A',
     price: 45000,
     description: 'Printer Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
-    type: 'Printer-Bw'
+    type: 'Printer-Bw',
+    linkDownload: ''
 }, {
     id: 9,
     img: 'asset/products/e2520AC,3025,4525.jpg',
     name: 'e-studio3025AC',
     price: 45000,
     description: 'Printer-color Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
-    type: 'Printer-color'
+    type: 'Printer-color',
+    linkDownload: ''
 }, {
     id: 10,
     img: 'asset/products/e2520AC,3025,4525.jpg',
     name: 'e-studio4525AC',
     price: 45000,
     description: 'Printer-color Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
-    type: 'Printer-color'
+    type: 'Printer-color',
+    linkDownload: ''
 }, {
     id: 11,
     img: 'asset/products//e2520AC,3025,4525.jpg',
     name: 'e-studio5015AC',
     price: 45000,
     description: 'Printer-color Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
-    type: 'Printer-color'
+    type: 'Printer-color',
+    linkDownload: ''
 }, {
     id: 12,
     img: 'asset/products/e-330AC 400AC.jpg',
     name: 'e-studio330AC 400AC',
     price: 45000,
     description: 'Printer-color Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
-    type: 'Printer-color'
+    type: 'Printer-color',
+    linkDownload: ''
 }, {
     id: 13,
     img: 'asset/products/PBS550.PNG',
     name: 'BPS-550',
     price: 110000,
     description: 'Blue-printer-solution Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
-    type: 'Blue-printer-solution'
+    type: 'Blue-printer-solution',
+    linkDownload: '1MN2_gH3FTK4VCrvCUzoePuBEu4-PN-m8'
 }, {
     id: 14,
     img: 'asset/products/bps-201.jpg',
     name: 'BPS-201',
     price: 45000,
     description: 'AURORA AS Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
-    type: 'Blue-printer-solution'
+    type: 'Blue-printer-solution',
+    linkDownload: ''
 }, {
     id: 15,
     img: 'asset/products/AS1610.jpg',
     name: 'AURORA AS-1610SB',
     price: 45000,
     description: 'AURORA AS Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
-    type: 'AURORA'
-}, {
-    id: 16, 
-    img: 'asset/products/4.png',
-    name: 'AURORA AS-1500CD',
-    price: 45000,
-    description: 'AURORA AS Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
-    type: 'AURORA'
-}, {
-    id: 17,
-    img: 'asset/products/4.png',
-    name: 'AURORA AS-1500CD',
-    price: 45000,
-    description: 'AURORA AS Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
-    type: 'AURORA'
-}, {
-    id: 18,
-    img: 'asset/products/4.png',
-    name: 'AURORA AS-1500CD',
-    price: 45000,
-    description: 'AURORA AS Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
-    type: 'AURORA'
-} ,{
-    id: 19,
-    img: 'asset/products/4.png',
-    name: 'AURORA AS-1500CD',
-    price: 45000,
-    description: 'AURORA AS Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
-    type: 'AURORA'
-}, {
-    id: 20,
-    img: 'asset/products/4.png',
-    name: 'AURORA AS-1500CD',
-    price: 45000,
-    description: 'AURORA AS Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
-    type: 'AURORA'
+    type: 'AURORA',
+    linkDownload: ''
 }];
+var selectProduct = 0;
 
 // [{},{},{}] // length = 3
 
@@ -201,12 +182,12 @@ function searchproduct(param) {
 var productindex = 0;
 function openProductDetail(index) {
     productindex = index;
-    console.log(productindex)
     $("#modalDesc").css('display', 'flex')
     $("#mdd-img").attr('src', product[index].img);
     $("#mdd-name").text(product[index].name)
     $("#mdd-price").text( numberWithCommas(product[index].price) + ' THB')
     $("#mdd-desc").text(product[index].description)
+    this.selectProduct = productindex;
 }
 
 function closeModal() {
@@ -214,36 +195,17 @@ function closeModal() {
 }
 
 var cart = [];
-function downloadPDF() {
-    var pass = true;
+function downloadPDF(index) {
+    console.log(index)
+    var pdfURL = `https://drive.google.com/uc?export=download&id=${product[index].linkDownload}`;
 
-    for (let i = 0; i < cart.length; i++) {
-        if( productindex == cart[i].index ) {
-            console.log('found same product')
-            cart[i].count++;
-            pass = false;
-        }
-    }
-
-    if(pass) {
-        var obj = {
-            index: productindex,
-            id: product[productindex].id,
-            name: product[productindex].name,
-            price: product[productindex].price,
-            img: product[productindex].img,
-            count: 1
-        };
-        // console.log(obj)
-        cart.push(obj)
-    }
-    console.log(cart)
-
-    Swal.fire({
-        icon: 'success',
-        title: 'Add ' + product[productindex].name + ' to cart !'
-    })
-    $("#cartcount").css('display','flex').text(cart.length)
+        // สร้างลิงก์สำหรับดาวน์โหลด PDF
+        var downloadLink = document.createElement("a");
+        downloadLink.href = pdfURL;
+        downloadLink.download = "document.pdf"; // ชื่อที่จะใช้สำหรับไฟล์ที่ดาวน์โหลด
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
 }
 
 function openCart() {
