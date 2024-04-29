64f05456c0e9c17fd28a6e52ef7e249f230d29aa
tanapon5444 <tanapon.mai.15@gmail.com>
2024-04-29 14:37:55 +0700

    แก้ไขหน้า

--- src/script.js
+++ src/script.js
@@ -2,104 +2,91 @@ var product = [{
     id: 1,
     img: 'asset/products/2329.bmp',
     name: 'e-studio2329A',
-    price: 7000,
     description: ' คุณสมบัติในการถ่ายเอกสาร 1.ความเร็วในการถ่ายเอกสาร 23 แผ่นต่อนาที (A4) 2.ใช้เวลาอุ่นเครื่อง 15 วินาที / หน่วยความจามาตรฐาน 512 MB 3.ความเร็วในการถ่ายเอกสารแผ่นแรก 6.4 วินาที 4.ความละเอียดในการถ่ายเอกสาร 600 x 600 dpi 5.สามารถถ่ายเอกสารต่อเนื่อง 1–999 แผ่นโดยอ่านต้นฉบับเพียงครั้งเดียว 6.หน้าจอแสดงผลแบบ LCD สามารถปรับความเข้มจางหน้าจอได้  7.ถาดบรรจุกระดาษ 250 แผ่น พร้อมถาดด้านข้างวางได้ 100 แผ่น 8.ปรับความเข้ม -จางอัตโนมัติและปรับตามต้องการได้ถึง 9 ระดับ 9.ระบบย่อ -ขยาย 25% - 400% และปรับเพิ่ม - ลดได้ครั้งละ 1% 10.รองรับต้นฉบับเอกสารและสาเนาตั้งแต่ขนาด A5 - A3 11.ใช้หมึกหมุนเวียน 100% ไม่มีกากหมึกทิ้ง 12.รองรับความหนาของกระดาษสาเนาได้ตั้งแต่ 52 - 216 แกรม 13.ชุดป้อนต้นฉบับหน้าหลังอัตโนมัติ (RADF) 50 แผ่น 14.มีชุดกลับสาเนาหน้าหลังอัตโนมัติ (Duplex) 15.สามารถปรินท์ผ่านระบบ WIFI ได้  16.มีฟังก์ชั่นถ่ายบัตรประชาชนหน้า-หลัง ลงสาเนาหน้าเดียวกัน (ID Card) 17.สามารถตั้งรหัสผู้ใช้งานได้ 99 รหัส โดยใช้เลขรหัส 5 หลัก (Department Code) 18.มีระบบถ่ายแยกเรียงชุดสาเนาเอกสารได้ถึง 999 ชุด พร้อมทั้งแบบทับซ้อนและไขว้ 90 องศา (Sort & Rotate Sort) คุณสมบัติในการปริ้นท์ 1.ความเร็วในการพิมพ์ 23 แผ่นต่อนาที (A4) 2.ความละเอียดในการพิมพ์ 2400 x 600 dpi (with smoothing) 3.สั่งสแกนเข้าโทรศัพท์มือถือผ่านสาย USB 4.ภาษาที่ใช้ในการพิมพ์เป็นระบบ Network LAN (10/100 Base T),USB 2.0 Hi Speed, PCL6 (PCL XL), PS3 (Option) 5.รองรับระบบปฏิบัติการ Windows 7, 8.1, 10 (32/64 bit),Server2016, 2012/R2(64 bit), 2008 SP2(32/64 bit),Mac OS 10.11/10.12/10.13/10.14 6.สามารถปรินท์เอกสารหน้า-หลังได้ ใหญ่สุดถึง A3 คุณสมบัติในการสแกนสีและขาว-ดำ 1. ความเร็วในการสแกน 25 แผ่น ต่อนาที (ขาว/ดำ) และ24 แผ่น ต่อนาที (สี), (A4) และสแกนเอกสารได้ถึงขนาด A3 2.ความละเอียดในการสแกน 600 x 600 dpi, สแกนเอกสารจัดเก็บ ใน USB Thumb Drive โดยตรง ทั้งสีและขาวดำ  3.คำสั่งในการสแกน Scan to File, Scan to E-mail, Scan to USB, Scan to SMB, FTP  4.ชนิดของไฟล์ ในการสแกน PDF, TIFF, JPEG.',
     type: 'Printer-Bw',
-    linkDownload: ''
+    linkDownload: '1GsLY-0Ee5xOPt5qAH_k3AatCWgJvYor8'
 }, {
     id: 2,
     img: 'asset/products/e2520AC,3025,4525.jpg',
     name: 'e-studio2520AC',
-    price: 1500,
     description: 'Printer-color Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
     type: 'Printer-color',
-    linkDownload: '18f1XgOxsBYNqOv920HZUBKdap2b5l1zt'
+    linkDownload: '18f1XgOxsBYNqOv920HZUBKdap2b5l1zt' 
 }, {
     id: 3,
     img: 'asset/products/BPS150.PNG',
     name: 'BPS-150',
-    price: 110000,
     description: 'Blue-printer-solution Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
     type: 'Blue-printer-solution',
-    linkDownload: ''
+    linkDownload: '1NLX3mAKxUrUNMpvSF8kBewLfOmngILC_'
 }, {
     id: 4,
     img: 'asset/products/as1500.png',
     name: 'AURORA AS-1500CD',
-    price: 45000,
     description: 'AURORA AS Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
     type: 'AURORA',
-    linkDownload: ''
+    linkDownload: '10xwuClzPGIebRpuUrLq_haoDFtiMsVZ5'
 }, {
     id: 5,
     img: 'asset/products/2523AD.jpg',
     name: 'e-studio2523AD',
-    price: 45000,
     description: 'Printer Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
     type: 'Printer-Bw',
-    linkDownload: ''
+    linkDownload: '1kwpjQiLbCzwKn61HtWLv0Mg0hd3xtIf'
 
 }, {
     id: 6,
     img: 'asset/products/e3028A.jpg',
     name: 'e-studio3028A',
-    price: 45000,
     description: 'Printer Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
     type: 'Printer-Bw',
-    linkDownload: ''
+    linkDownload: '1MPfVp2Xn4DshBWoQWh9Si3F3BksNSkwm'
 }, {
     id: 7,
     img: 'asset/products/e4528A.jpg',
     name: 'e-studio4528A',
-    price: 45000,
     description: 'Printer Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
     type: 'Printer-Bw',
-    linkDownload: ''
+    linkDownload: '1ZbE6rOfpnNRQW0UOCcDxNlBqA5eAmA2Z'
 }, {
     id: 8,
     img: 'asset/products/e5528A.jpg',
     name: 'e-studio5528A',
-    price: 45000,
     description: 'Printer Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
     type: 'Printer-Bw',
-    linkDownload: ''
+    linkDownload: '1NZLqQKscoTBR5q6O5KDkrG7DEM2heLXi'
 }, {
     id: 9,
     img: 'asset/products/e2520AC,3025,4525.jpg',
     name: 'e-studio3025AC',
-    price: 45000,
     description: 'Printer-color Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
     type: 'Printer-color',
-    linkDownload: ''
+    linkDownload: '1XdNFamn66ZGCDK4pVlpfvWuIuyu6Shn3'
 }, {
     id: 10,
     img: 'asset/products/e2520AC,3025,4525.jpg',
     name: 'e-studio4525AC',
-    price: 45000,
     description: 'Printer-color Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
     type: 'Printer-color',
-    linkDownload: ''
+    linkDownload: '11lHBKjg6fjAGrndd2hXIrOb4c9NPhI9b'
 }, {
     id: 11,
     img: 'asset/products//e2520AC,3025,4525.jpg',
     name: 'e-studio5015AC',
-    price: 45000,
     description: 'Printer-color Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
     type: 'Printer-color',
-    linkDownload: ''
+    linkDownload: '1dszsc8iOuu_-KRJ8ofNQ_S4_jpSXR0Wx'
 }, {
     id: 12,
     img: 'asset/products/e-330AC 400AC.jpg',
     name: 'e-studio330AC 400AC',
-    price: 45000,
     description: 'Printer-color Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
     type: 'Printer-color',
-    linkDownload: ''
+    linkDownload: '1XdO7d3Zq0Ia4OOsV1EKDB8wZ97eXiNSa'
 }, {
     id: 13,
     img: 'asset/products/PBS550.PNG',
     name: 'BPS-550',
-    price: 110000,
     description: 'Blue-printer-solution Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
     type: 'Blue-printer-solution',
     linkDownload: '1MN2_gH3FTK4VCrvCUzoePuBEu4-PN-m8'
@@ -107,18 +94,16 @@ var product = [{
     id: 14,
     img: 'asset/products/bps-201.jpg',
     name: 'BPS-201',
-    price: 45000,
     description: 'AURORA AS Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
     type: 'Blue-printer-solution',
-    linkDownload: ''
+    linkDownload: '1seFA8TyHYmLUfdDpDGTGREhoWAWhtPM7'
 }, {
     id: 15,
     img: 'asset/products/AS1610.jpg',
     name: 'AURORA AS-1610SB',
-    price: 45000,
     description: 'AURORA AS Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aliquam debitis sapiente magnam consequuntur libero.',
     type: 'AURORA',
-    linkDownload: ''
+    linkDownload: '1b5JTdVo-50HTQRdV1mOVkRkCVEneX91h'
 }];
 var selectProduct = 0;
 
@@ -130,7 +115,7 @@ $(document).ready(() => {
         html += `<div onclick="openProductDetail(${i})" class="product-items ${product[i].type}">
                     <img class="product-img" src="${product[i].img}" alt="">
                     <p style="font-size: 1.2vw;">${product[i].name}</p>
-                    <p stlye="font-size: 1vw;">${ numberWithCommas(product[i].price) } THB</p>
+                   
                 </div>`;
     }
     $("#productlist").html(html);
